49978c25cb7e12fb8874024eb7e2e7d35c51aff6
riinuots <riinuots@gmail.com>
2019-07-10 10:05:59 +0100

    includes code for building a single chapter in the _bookdown.yml. Tables still break in 03

---
+++ healthyr_theme.R
@@ -0,0 +1,8 @@
+ggplot <- function(...) ggplot2::ggplot(...) +
+  scale_fill_brewer(palette="Paired") +
+  scale_colour_brewer(palette="Paired") +
+  theme_bw() +
+  theme(legend.position='right') +
+  guides(fill=guide_legend(ncol=1))
+
+suppressPackageStartupMessages(library(tidyverse))  #this loads a list of helpful packages

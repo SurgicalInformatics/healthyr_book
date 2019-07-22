c3930ad71fc43c54e7a9510744d183fd95d06472
riinuots <riinuots@gmail.com>
2019-07-22 20:27:25 +0100

    operators

---
+++ data/gbd_process.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+gbd_short = read_csv("data/IHME-GBD_2017_DATA-short.csv") %>%
+  select(-location, -sex, -age, -upper, -lower, -metric, -measure) %>%
+  rename(deaths = val) %>%
+  mutate(cause = fct_recode(cause, "Communicable diseases" = "Communicable, maternal, neonatal, and nutritional diseases")) %>%
+  arrange(year, cause)
+
+gbd_short %>%
+  write_csv(path = "data/global_burden_disease_SHORT.csv")
+

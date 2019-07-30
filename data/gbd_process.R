88117d6f8b6facd95c00042cef5e0af1e7cb0682
Riinu Ots <riinu.ots@gmail.com>
2019-07-30 17:07:54 +0100

    add new vars and paste done, joins todo

--- data/gbd_process.R
+++ data/gbd_process.R
@@ -3,6 +3,7 @@ library(tidyverse)
 gbd_short = read_csv("data/IHME-GBD_2017_DATA-short.csv") %>%
   select(-location, -sex, -age, -upper, -lower, -metric, -measure) %>%
   rename(deaths = val) %>%
+  mutate(deaths = round(deaths, 0)) %>%
   mutate(cause = fct_recode(cause, "Communicable diseases" = "Communicable, maternal, neonatal, and nutritional diseases")) %>%
   arrange(year, cause)
 
